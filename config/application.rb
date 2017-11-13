3b5641b5b4a7fcb71b67cb7b1dc6aac2adea266c
Jamie Kite <jgkite@github.com>
2017-11-13 13:52:52 -0500

    Add Primer CSS

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ Bundler.require(*Rails.groups)
 
 module GitGrub
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join("node_modules")
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
