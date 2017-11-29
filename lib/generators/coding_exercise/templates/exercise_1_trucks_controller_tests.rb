12880cbc28d232000e8af336906e7e7747c4a3da
Jamie Kite <jgkite@github.com>
2017-11-29 08:54:28 -0500

    This, too!

--- lib/generators/coding_exercise/templates/exercise_1_trucks_controller_tests.rb
+++ lib/generators/coding_exercise/templates/exercise_1_trucks_controller_tests.rb
@@ -32,7 +32,7 @@ class Api::TrucksControllerTest < ActionDispatch::IntegrationTest
     get api_truck_url(truck)
 
     assert_response 200
-    assert_equal "5.0", response[:rating]
+    assert_equal 5.0, response[:rating]
   end
 
   test "Decimals should be rounded to half a star when multiple ratings exist" do
