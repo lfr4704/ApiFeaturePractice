db0680822bb9a7a8793e5716ecd660ab2da5bad1
Computer <computer@Computers-MacBook-Air.local>
2018-08-16 15:54:28 -0700

    added controllers, views and models for the rating feature

---
+++ app/models/rating.rb
@@ -0,0 +1,5 @@
+class Rating < ApplicationRecord
+
+	validates :title, presence: true,
+						length: { minimum: 5}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'ratings/index'
+
+  get 'ratings/show'
+
+  get 'ratings/new'
+
+  get 'ratings/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
